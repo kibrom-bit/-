0c8f0c1c9fd9805a9df9f5ff381e94c046020b75
kebi <kibromabebe20@gmail.com>
2026-02-09 15:21:10 -0900

    chore: configure Tailwind CSS with custom settings

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        ethiopianGold: '#D4AF37', // Yellow/Gold
+        ethiopianRed: '#DA1A32',  // Red
+        paperWhite: '#F9F7F2',    // Background
+      }
+    },
+  },
+  plugins: [],
+}
